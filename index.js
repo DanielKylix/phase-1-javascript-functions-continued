9ecc4e6ef0c38215e6b4d83489c246d05b25c4eb
Daniel <daniel@kylix.online>
2022-08-16 12:57:39 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const saturdayFun = (activity = "roller-skate") => {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (activity = "go to the office") => {
+    return activity = `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (symbol = "*") => {
+    return function adjective(sentence ="special") {
+        return `You are ${symbol}${sentence}${symbol}!`
+    }
+}
